d9b222e41516c2b29b6990c17a05955b95fcaaf4
Zach Johnson <zjohnson@omnitech-inc.com>
2020-05-11 14:51:17 -0500

    Added and setup tailwind

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="max-w-full p-1">
+
+      <CalendarItem date={1} />
+
     </div>
+  );
+}
+
+function CalendarItem(props) {
+  return (
+  <div className="h-24 w-16 border-2 border-black bg-gray-100 px-1">{props.date}</div>
   );
 }
 
